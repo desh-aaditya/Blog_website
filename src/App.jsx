3650f0f959892c4b48b4a36fab66e2cbaff8703c
desh-aaditya <desh.aaditya15@gmail.com>
2026-06-13 23:07:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+
+  return (
+   <>
+   <h1>Hello</h1>
+   </>
+  )
+}
+
+export default App
