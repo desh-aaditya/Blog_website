02a247a10aa90ea263ad2ebf895ee88b3c0200d5
desh_aaditya <desh.aaditya165@gmail.com>
2026-06-16 11:53:08 +0530

    Add PostForm and RTE components for post creation and editing functionality

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutButton from "./Header/LogoutButton";
+import RTE from "./RTE";
 export{
-    Header,Footer,Container,Logo,LogoutButton
+    Header,Footer,Container,Logo,LogoutButton,RTE
 }
